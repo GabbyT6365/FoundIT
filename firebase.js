dfb6de6462f31aa9e066b505f046deeed6869ad8
GabbyT6365 <gabtribiana@gmail.com>
2026-03-03 21:23:18 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,33 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword }
+from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC4XLnRI7zIRc2xnkSpj1kkk-qEkkXiOM",
-  authDomain: "foundit-e6d39.firebaseapp.com",
-  projectId: "foundit-e6d39",
-  storageBucket: "foundit-e6d39.firebasestorage.app",
-  messagingSenderId: "169636403427",
-  appId: "1:169636403427:web:bb745e38def8690ea00dd6",
-  measurementId: "G-T30WLJNQ2P"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+window.registerUser = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => alert("Account created successfully!"))
+    .catch(error => alert(error.message));
+};
+
+window.loginUser = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => alert("Login successful!"))
+    .catch(error => alert(error.message));
+};
